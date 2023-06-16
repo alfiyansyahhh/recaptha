17fc79fac5c627bc23091ed2de3705044717a98b
ilham_alfiyansyah <ilham@loyalto.id>
2023-06-16 14:51:26 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,68 @@
-import { useState } from 'react'
+import React, { useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 
-function App() {
+function LoginForm() {
+  const [sitekey, setSitekey] = useState("");
+  const [showReCAPTCHA, setShowReCAPTCHA] = useState(false);
   const [isCaptchaVerified, setIsCaptchaVerified] = useState(false);
-
-  const handleSubmit = (event) => {
-    if (isCaptchaVerified) {
-      alert('verifikasi berhasil')
-    } else {
-      alert("Harap verifikasi reCAPTCHA");
-    }
-  };
 
   const handleCaptchaVerify = (token) => {
     setIsCaptchaVerified(true);
     console.log(token, 'ini token')
   };
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-      <ReCAPTCHA sitekey="6Lf2ZKEmAAAAAKe7IK7_BaTu_twXi9ujhVLfK_74"
-      onChange={handleCaptchaVerify}
-      type="audio"
-      />
-      <button type="submit">Login</button>
-    </form>
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    </>
-  )
+    if (isCaptchaVerified) {
+      alert('isverifikasi')
+      // Lakukan logika pengiriman data login ke server atau validasi lainnya
+    } else {
+      alert("Harap verifikasi reCAPTCHA");
+    }
+  };
+
+  const handleShowReCAPTCHA = () => {
+    if(sitekey !== '') {
+      setShowReCAPTCHA(true)
+    } 
+  }
+
+  {/* 6Lf2ZKEmAAAAAKe7IK7_BaTu_twXi9ujhVLfK_74 */}
+
+
+  return (
+    <div style={{width: '100%', justifyContent: 'center', alignItems: 'center'}}>
+      {/* <form onSubmit={handleSubmit}> */}
+        <div>
+          <label>Sitekey:</label>
+          <br/>
+          <input
+            type="text"
+            value={sitekey}
+            onChange={(e) => setSitekey(e.target.value)}
+            />
+            <br/>
+        </div>
+
+            <br/>
+        <button onClick={handleShowReCAPTCHA}>showReCAPTCHA</button>
+            <br/>
+
+            <br/>
+            {showReCAPTCHA && 
+            <ReCAPTCHA
+            sitekey={sitekey || ''}
+            onChange={handleCaptchaVerify}
+            type="audio"
+            />
+            } 
+          <br/>
+
+        <button onClick={handleSubmit}>Login</button>
+      {/* </form> */}
+    </div>
+  );
 }
 
-export default App
+export default LoginForm;

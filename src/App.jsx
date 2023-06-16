b60058234e0e4aac28e40b7fea412fbceb51c70a
ilham_alfiyansyah <ilham@loyalto.id>
2023-06-16 14:58:18 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,17 @@ function LoginForm() {
   const [isCaptchaVerified, setIsCaptchaVerified] = useState(false);
 
   const handleCaptchaVerify = (token) => {
-    setIsCaptchaVerified(true);
-    console.log(token, 'ini token')
+    if (!isCaptchaVerified) {
+      setIsCaptchaVerified(true);
+      console.log(token, "ini token");
+    }
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     if (isCaptchaVerified) {
-      alert('isverifikasi')
+      alert("Verifikasi berhasil");
       // Lakukan logika pengiriman data login ke server atau validasi lainnya
     } else {
       alert("Harap verifikasi reCAPTCHA");
@@ -23,44 +25,35 @@ function LoginForm() {
   };
 
   const handleShowReCAPTCHA = () => {
-    if(sitekey !== '') {
-      setShowReCAPTCHA(true)
-    } 
-  }
-
-  {/* 6Lf2ZKEmAAAAAKe7IK7_BaTu_twXi9ujhVLfK_74 */}
-
+    if (sitekey !== "") {
+      setShowReCAPTCHA(true);
+    }
+  };
 
   return (
-    <div style={{width: '100%', justifyContent: 'center', alignItems: 'center'}}>
-      {/* <form onSubmit={handleSubmit}> */}
-        <div>
-          <label>Sitekey:</label>
-          <br/>
-          <input
-            type="text"
-            value={sitekey}
-            onChange={(e) => setSitekey(e.target.value)}
-            />
-            <br/>
-        </div>
+    <div style={{ width: "100%", justifyContent: "center", alignItems: "center" }}>
+      <div>
+        <label>Sitekey:</label>
+        <br />
+        <input type="text" value={sitekey} onChange={(e) => setSitekey(e.target.value)} />
+        <br />
+      </div>
 
-            <br/>
-        <button onClick={handleShowReCAPTCHA}>showReCAPTCHA</button>
-            <br/>
+      <br />
+      <button onClick={handleShowReCAPTCHA}>Show reCAPTCHA</button>
+      <br />
 
-            <br/>
-            {showReCAPTCHA && 
-            <ReCAPTCHA
-            sitekey={sitekey || ''}
-            onChange={handleCaptchaVerify}
-            type="audio"
-            />
-            } 
-          <br/>
+      <br />
+      {showReCAPTCHA && (
+        <ReCAPTCHA
+          sitekey={sitekey || ""}
+          onChange={handleCaptchaVerify}
+          type="image"
+        />
+      )}
+      <br />
 
-        <button onClick={handleSubmit}>Login</button>
-      {/* </form> */}
+      <button onClick={handleSubmit}>Login</button>
     </div>
   );
 }
